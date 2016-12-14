990c74eeff7d97500d87bc175f99d26d1af928fe
TheSwagMan <theswagman@gmx.fr>
2016-12-14 15:30:09 +0100

    Setting up...

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.0.1',
-    description='Sample package for Python-Guide.org',
+    name='pywebcrawler',
+    version='0.1',
+    description='Python Web Crawler',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Potier Thomas',
+    author_email='theswagman@gmx.fr',
+    url='https://github.com/TheSwagMan/WebCrawler',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
